b2e65e6677889432acbf4d132da53a1ef40dc3dd
Gustavo Carvalho <gustavo.decarvalhogm@gmail.com>
2026-09-17 16:29:29 -0300

    novos exercicios

---
+++ Estudos-Python/Exercicios-Modularizacao/ex115/sistema.py
@@ -0,0 +1,28 @@
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt.'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        #Opção de listar o conteúdo do arquivo!
+        lerArquivo(arq)
+        
+    elif resposta == 2:
+        #Opção de cadastrar uma nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabeçalho('Saindo do Sistema... Até logo!')
+        break
+    else:
+        print('\033[31mErro! Digite uma opção válida.\033[m')
+    sleep(2)
